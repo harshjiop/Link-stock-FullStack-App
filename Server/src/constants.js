5829a9ff349e259eed39bac4929a4b2d4631673d
harshjiop <hk8051871496@gmail.com>
2024-01-20 13:45:48 +0530

    atlesh databesh setup

--- Server/src/constants.js
+++ Server/src/constants.js
@@ -1,10 +1,11 @@
-export const DB_NAME = "LinkStockBackend";
+export const DB_NAME = "NewLinkStock";
+// export const DB_URL =
+//   "mongodb+srv://Harsh:Harsh123@youtube-backend.n9qz6ew.mongodb.net";
 export const DB_URL =
-  "mongodb://127.0.0.1:27017";
+  "mongodb+srv://Harsh:Harsh123@newlinkstock.xe5pugx.mongodb.net";
 export const PORT = 8000;
-export const Cors_Origin = "";
-export const ACCESS_TOKEN_SECRET =
-  "mongodb://localhost:27017";
+export const Cors_Origin = "http://localhost:3000/";
+export const ACCESS_TOKEN_SECRET = "mongodb://localhost:27017";
 export const ACCESS_TOKEN_EXPIRY = "1d";
 export const REFRESH_TOKEN_SECRET = "123";
 export const REFRESH_TOKEN_EXPIRY = "10d";
