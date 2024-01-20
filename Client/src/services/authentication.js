cd813c4b672972ba3eb29c20bbd728cb2424ec3c
Lokesh Ghosh <lghosh857@gmail.com>
2024-01-20 16:37:35 +0530

    client update : register & login service

--- Client/src/services/authentication.js
+++ Client/src/services/authentication.js
@@ -1,3 +1,47 @@
-class Authentication{
-    
+class Authentication {
+    async signUp(data) {
+        console.log(data)
+        try {
+            const response = await fetch('http://localhost:8000/api/v1/users/register', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({ username: data.username, fullName: data.fullName, email: data.email, password: data.password }),
+            })
+
+            if (response.ok) {
+                console.log('success', response)
+                return true
+            } else {
+                console.log('some issue', response)
+                return false;
+            }
+        } catch (error) {
+            console.log('some error in catch', error)
+            return error;
+        }
+    }
+
+    async login(data) {
+        try {
+            const response = await fetch('http://localhost:8000/api/v1/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({ username: data.userName, password: data.password }),
+            })
+            if (response.ok) {
+                return response
+            } else {
+                console.log('some issue', response)
+            }
+        } catch (error) {
+            console.log('some error in catch', error)
+            return error.message;
+        }
+    }
 }
+
+export default new Authentication;
