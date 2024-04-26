968ec4ff806f05d5bb11f1b0743e544fde8ccf1d
Lokesh Ghosh <lghosh857@gmail.com>
2024-04-27 03:12:44 +0530

    client update : updated loader

--- Client/src/pages/loader/Loader.jsx
+++ Client/src/pages/loader/Loader.jsx
@@ -1,7 +1,67 @@
 import "./Loader.css";
 export default function Loader() {
   return (
-    <div className=" backdrop-blur-md h-screen w-screen bg-center bg-no-repeat bg-cover bg-slate-900 relative  flex justify-center items-center">
+    <div className="  h-screen flex justify-center items-center bg-slate-900">
+      <div
+        className="hexagon flex justify-center items-center mx-auto"
+        aria-label="Animated hexagonal ripples"
+      >
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+        <div className="hexagon__group">
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+          <div className="hexagon__sector"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function test() {
+  return (
+    <div className="  backdrop-blur-md h-screen w-screen bg-center bg-no-repeat bg-cover bg-slate-900 relative  flex justify-center items-center">
       {/* spinner container */}
       <div className="">
         {/* spinner section */}
